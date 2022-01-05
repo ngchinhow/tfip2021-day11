f12b771622f66f5b98285b6b266b0059e707bf4d
ngchinhow <ngchinhow@gmail.com>
2022-01-05 16:29:04 +0800

    added order of preference for port number

--- src/main/java/com/tfip2021/module2/Workshop11Application.java
+++ src/main/java/com/tfip2021/module2/Workshop11Application.java
@@ -1,13 +1,33 @@
 package com.tfip2021.module2;
 
+import java.util.Collections;
+
+// import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Workshop11Application {
+	private static final Logger logger = LoggerFactory.getLogger(Workshop11Application.class);
+	private static final String DEFAULT_PORT = "3000";
 
 	public static void main(String[] args) {
-		SpringApplication.run(Workshop11Application.class, args);
+		logger.info("Workshop 11");
+		SpringApplication app = new SpringApplication(Workshop11Application.class);
+		String port = System.getProperty("PORT", DEFAULT_PORT);
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+		if (cliOpts.containsOption("port")) {
+			port = cliOpts.getOptionValues("port").get(0);
+		}
+		logger.debug(port);
+		app.setDefaultProperties(
+			Collections.singletonMap("server.port", port)
+		);
+		app.run(args);
 	}
 
 }
