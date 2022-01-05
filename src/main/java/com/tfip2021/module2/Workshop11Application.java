086d0e1df4a8719b44b1a2664112fe4b729630ad
ngchinhow <ngchinhow@gmail.com>
2022-01-05 14:18:51 +0800

    Added index.html

--- src/main/java/com/tfip2021/module2/Workshop11Application.java
+++ src/main/java/com/tfip2021/module2/Workshop11Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Workshop1Application {
+public class Workshop11Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Workshop1Application.class, args);
+		SpringApplication.run(Workshop11Application.class, args);
 	}
 
 }
